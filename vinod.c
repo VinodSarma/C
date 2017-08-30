b12cc56db858906546e4af51f3fc3c5acbcfca7a
VinodSarma <vinodpbhatla@gmail.com>
2017-08-30 15:42:28 +0530

    Update vinod.c
    extra printf

--- vinod.c
+++ vinod.c
@@ -2,6 +2,7 @@
 void main()
 {
 printf("hello world with jenkins\n");
-  printf("Changeing the file to test if triggering is happening in jenkins\n");
+  printf("Changeing the file to test if triggering is happening in jenkins \n");
+  printf("new printf\n");
   exit(0);
 }
