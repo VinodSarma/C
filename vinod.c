8b50a2e69c321af3da45820023cc3ff7d58b7eb4
VinodSarma <vinodpbhatla@gmail.com>
2017-08-30 15:51:05 +0530

    Update vinod.c
    Polling test

--- vinod.c
+++ vinod.c
@@ -3,6 +3,6 @@ void main()
 {
 printf("hello world with jenkins\n");
   printf("Changeing the file to test if triggering is happening in jenkins \n");
-  printf("new printf\n");
+  printf("new printf for polling\n");
   exit(0);
 }
